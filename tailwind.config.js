f164fcd1e8c704cd44af74a975ea543258c5b50f
NgouanKoffi <163684599+NgouanKoffi@users.noreply.github.com>
2024-05-09 13:44:56 +0000

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,14 @@ module.exports = {
   purge: ['./src/**/*.{html,ts}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'custom-804': '819px',
+      },
+      colors: {
+        'custom-yellow': '#efbc46', // Vous pouvez nommer la couleur comme vous le souhaitez
+      }
+    },
   },
   variants: {
     extend: {},
